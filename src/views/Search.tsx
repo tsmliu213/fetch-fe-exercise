8d0849d7e3779c749a0918c4a65513105044f95d
Thomas Liu <thomasl@hey.com>
2025-02-04 19:23:32 -0800

    update heart icon

--- src/views/Search.tsx
+++ src/views/Search.tsx
@@ -143,10 +143,12 @@ export default function Search({ selectedBreeds, onBreedsChange, favoriteDogs, o
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className={`h-6 w-6 ${favoriteDogs.some(d => d.id === dog.id) ? 'text-red-500 fill-current' : 'text-gray-400'}`}
+                  className={`h-6 w-6 ${favoriteDogs.some(d => d.id === dog.id) ? 'text-red-500 fill-current' : 'text-black stroke-current'}`}
                   viewBox="0 0 24 24"
                 >
-                  <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+                  <path d={favoriteDogs.some(d => d.id === dog.id) ? 
+                    "M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" :
+                    "M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35zM12 4.79C10.91 3.69 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3c-1.74 0-3.41.81-4.5 2.09"} />
                 </svg>
               </button>
               <img 
